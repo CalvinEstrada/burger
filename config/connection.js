22e981ab0815a9991adce72992be840cf7340944
Calvin <calvinnoestr@gmail.com>
2021-03-17 18:58:05 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 // Set up MySQL connection.
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 let connection;
 
@@ -7,18 +7,19 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
     port: 3306,
-    user: 'root',
-    password: '',
-    database: 'burgers_db',
+    user: "root",
+    password: "",
+    database: "burgers_db",
   });
 }
 
 // Make connection.
 connection.connect(function(err) {
   if (err) {
-    console.error(`error connecting: ${err.stack}`);
+    console.error(`error connecting: 
+    ${err.stack}`);
     return;
   }
   console.log(`connected as id ${connection.threadId}`);
