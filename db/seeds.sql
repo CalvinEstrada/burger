11065ff31a5dcef4bf3bc69c8c2a99a05471460b
CalvinEstrada <70289312+CalvinEstrada@users.noreply.github.com>
2021-03-17 12:16:48 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured)
+VALUES
+("Cheesburger", false),
+("MushroomNSwiss Burger", false),
+("Black Bean Burger", false),
+("Plain Burger", false),
+("Bacon Cheeseburger", false);
