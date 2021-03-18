49cad18bff24ea1d569c01463f47a81e2afc4237
Calvin <calvinnoestr@gmail.com>
2021-03-17 19:09:50 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,6 @@ const mysql = require("mysql");
 
 let connection;
 
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
