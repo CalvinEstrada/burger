277b1e2d541c27a26d6109ab95ab9c7dc37c6756
Calvin <calvinnoestr@gmail.com>
2021-03-17 19:07:07 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,30 @@ const mysql = require("mysql");
 
 let connection;
 
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: "	xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    host: "localhost",
     port: 3306,
-    user: "e0dcwwk68ifva4w4",
-    password: "	xms0bhfkxdq45kp1",
+    user: "root",
+    password: "",
     database: "burgers_db",
   });
-}
+}git 
 
 // Make connection.
 connection.connect(function(err) {
